ef199d391276bdb3d2dae638ebf6ce92e3189589
MAOZ <maooz4426@gmail.com>
2025-05-02 16:41:17 +0900

    わかりやすいように文章を作成

--- yarn/src/app/page.tsx
+++ yarn/src/app/page.tsx
@@ -3,6 +3,6 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div>deploy by yarn test</div>
+    <div>Deploy by yarn test Check for working workflow</div>
   );
 }
